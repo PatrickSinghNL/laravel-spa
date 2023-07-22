6b03e0b7446b7c4765d636017b91fb34ac40ce7d
Patrick Singh <patrick.singh@nh1816.nl>
2023-07-22 23:52:08 +0200

    Added category filter and sorting

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,12 +5,27 @@ namespace App\Http\Controllers\Api;
 use App\Models\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Database\Eloquent\Builder;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with('category')->paginate(10);
+        $orderColumn = request('order_column', 'created_at');
+        if (! in_array($orderColumn, ['id', 'title', 'created_at'])) {
+            $orderColumn = 'created_at';
+        }
+        $orderDirection = request('order_direction', 'desc');
+        if (! in_array($orderDirection, ['asc', 'desc'])) {
+            $orderDirection = 'desc';
+        }
+
+        $posts = Post::with('category')
+            ->when(request('category'), function (Builder $query) {
+                $query->where('category_id', request('category'));
+            })
+            ->orderBy($orderColumn, $orderDirection)
+            ->paginate(10);
 
         return PostResource::collection($posts);
     }
